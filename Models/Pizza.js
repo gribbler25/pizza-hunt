5d0247f8753d32b4dc54a76890364dadc0f54b93
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-09-18 16:39:33 -0500

    pizza, comment routes good and pizza data populating to the front end

--- Models/Pizza.js
+++ Models/Pizza.js
@@ -1,23 +1,45 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  //a virtual keeps track of computed value that  gets evaluated when you try to access the model's properties, this property is not stored inthe dataBase!
+  return this.comments.length;
 });
 
-const Pizza = model("pizza", PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
